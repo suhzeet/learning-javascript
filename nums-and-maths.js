6fd5187219ff97f839a5a01bfde59a3bd211755a
suhzeet <sujeetneupane28@gmail.com>
2024-05-19 22:40:57 +0545

    learning nums and maths

---
+++ nums-and-maths.js
@@ -0,0 +1,23 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.53;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// 
+// MATHS
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+console.log(Math.random()*10 + 1);
