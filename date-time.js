a1f608d1c0cc5b0ca87470ff3fd62adc0da17232
suhzeet <sujeetneupane28@gmail.com>
2024-05-20 22:52:41 +0545

    about date and time

---
+++ date-time.js
@@ -0,0 +1,21 @@
+// dates
+
+let myDate = new Date();
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 1, 23)
+let myCreatedDate = new Date("01-03-2023");
+// console.log(myCreatedDate.toDateString());
+
+// time
+
+// let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp, myCreatedDate.getTime())
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay()); 
