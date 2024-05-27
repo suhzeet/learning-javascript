5593d632fa5b0b60823dafeabb03aa6118dea083
suhzeet <sujeetneupane28@gmail.com>
2024-05-27 20:45:25 +0545

    added objects file

---
+++ objects.js
@@ -0,0 +1,11 @@
+const jsUser = {
+    name: "Sujit",
+    age: 21,
+    location: "Chitwan",
+    email: "example@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ['Monday', 'Saturday']
+};
+
+console.log(jsUser.email);
+console.log(jsUser[email]);
