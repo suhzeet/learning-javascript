76d90ce20a03fb6ea42be0478ae280e6d44b2920
suhzeet <sujeetneupane28@gmail.com>
2024-06-02 21:11:23 +0545

    methods in objects

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
+const mySym = Symbol('key1');
 const jsUser = {
     name: "Sujit",
+    [mySym]: 'mykey1',
     age: 21,
     location: "Chitwan",
     email: "example@gmail.com",
@@ -7,5 +9,20 @@ const jsUser = {
     lastLoginDays: ['Monday', 'Saturday']
 };
 
-console.log(jsUser.email);
-console.log(jsUser[email]);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+
+// console.log(typeof jsUser.mySym);
+
+jsUser.greeting = function() {
+    console.log("hello js user");
+}
+
+jsUser.greetingTwo = function() {
+    console.log(`hello js user, ${this.name}`);
+}
+
+// console.log(jsUser.greeting())
+// console.log(jsUser.greetingTwo())
+jsUser.greeting()
+jsUser.greetingTwo()
