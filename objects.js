1427f8e45e772a2db50da95ca35d80a54503face
suhzeet <sujeetneupane28@gmail.com>
2024-06-10 16:07:12 +0545

    updated

--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
-const mySym = Symbol('key1');
+const mySym = Symbol("key1");
 const jsUser = {
-    name: "Sujit",
-    [mySym]: 'mykey1',
-    age: 21,
-    location: "Chitwan",
-    email: "example@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays: ['Monday', 'Saturday']
+  name: "Sujit",
+  [mySym]: "mykey1",
+  age: 21,
+  location: "Chitwan",
+  email: "example@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
 };
 
 // console.log(jsUser.email);
@@ -14,19 +14,17 @@ const jsUser = {
 
 // console.log(typeof jsUser.mySym);
 
-jsUser.greeting = function() {
-    // console.log("hello js user");
-}
+jsUser.greeting = function () {
+  // console.log("hello js user");
+};
 
-jsUser.greetingTwo = function() {
-    // console.log(`hello js user, ${this.name}`);
-}
+jsUser.greetingTwo = function () {
+  // console.log(`hello js user, ${this.name}`);
+};
 
 // console.log(jsUser.greeting())
 // console.log(jsUser.greetingTwo())
 // jsUser.greeting()
 // jsUser.greetingTwo()
 
-const tinderUser = {}
-
-tinderUser.id = 
+const tinderUser = {};
